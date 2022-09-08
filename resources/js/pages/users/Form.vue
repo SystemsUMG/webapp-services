593ff763fcd594c3381b4cc2e3fe8fd3844e9733
Cristian González <crisgon1065@gmail.com>
2022-09-08 17:41:57 -0600

    Se agregaron select dinamicos en form usuario

--- resources/js/pages/users/Form.vue
+++ resources/js/pages/users/Form.vue
@@ -3,16 +3,13 @@
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <form @submit.prevent="SEND()">
-                    <div class="d-flex align-items-start px-3 py-1">
+                    <div class="d-flex align-items-start px-3">
                         <button type="button " class="btn-close text-dark" @click="CLOSE()">
                             <i class="fa fa-times"></i>
                         </button>
-                        <button type="submit" class="btn btn-primary btn-sm ms-auto mt-2 mb-0">
-                            <i class="fas fa-spinner fa-spin me-2" v-if="load"></i>
-                            <span>Guardar</span>
-                        </button>
                     </div>
                     <div class="modal-body pt-0 px-4">
+                        <hr class="horizontal dark"/>
                         <p class="text-uppercase text-sm">Información Personal</p>
                         <div class="row">
                             <div class="col-md-6">
@@ -46,13 +43,19 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="rol_id" class="form-control-label">Puesto</label>
-                                    <input id="rol_id" class="form-control" type="text" v-model="data.rol_id">
+                                    <select  id="rol_id" class="form-select" v-model="data.rol_id">
+                                        <option value="" disabled hidden>Seleccione una opción</option>
+                                        <option v-for="rol in roles" :value="rol.id" :key="rol.id">{{ rol.name }}</option>
+                                    </select>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="department_id" class="form-control-label">Área</label>
-                                    <input id="department_id" class="form-control" type="text" v-model="data.department_id">
+                                    <select  id="department_id" class="form-select" v-model="data.department_id">
+                                        <option value="" disabled hidden>Seleccione una opción</option>
+                                        <option v-for="department in departments" :value="department.id" :key="department.id">{{ department.name }}</option>
+                                    </select>
                                 </div>
                             </div>
                             <div class="col-md-6">
@@ -64,8 +67,25 @@
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label for="region_id" class="form-control-label">Región</label>
-                                    <input id="region_id" class="form-control" type="number" v-model="data.region_id">
+                                    <select  id="region_id" class="form-select" v-model="data.region_id">
+                                        <option value="" disabled hidden>Seleccione una opción</option>
+                                        <option v-for="region in regions" :value="region.id" :key="region.id">{{ region.name }}</option>
+                                    </select>
                                 </div>
+                            </div>
+                        </div>
+                        <div class="row justify-content-center">
+                            <div class="col mb-2 me-0 text-end">
+                                <button @click="CLOSE()" type="button" class="btn btn-secondary btn-sm ms-auto mt-2 mb-0">
+                                    <i class="fas fa-times"></i>
+                                    <span class="ms-2">Cancelar</span>
+                                </button>
+                            </div>
+                            <div class="col mb-2 ms-0 text-start">
+                                <button type="submit" class="btn btn-primary btn-sm ms-auto mt-2 mb-0">
+                                    <i class="fas fa-spinner fa-spin me-2" v-if="load"></i>
+                                    <span>Guardar</span>
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -79,6 +99,9 @@ export default {
     props: ['open', 'method', 'id'],
     data(){
         return {
+            icon: '',
+            message: '',
+            loader: {},
             data: {
                 name: '',
                 email: '',
@@ -89,21 +112,33 @@ export default {
                 rol_id: '',
                 department_id: ''
             },
-            message: '',
+            roles: [],
+            departments: [],
+            regions: [],
             load: false,
             count: 0,
             url: '',
         }
     },
     computed: {
-        OPEN: function(){
+        OPEN: function() {
             let _this = this
+            _this.loadData('rols')
+            _this.loadData('departments')
+            _this.loadData('regions')
             if(_this.method == 'PUT') {
-                axios({url: '/users/'+ _this.id, method: 'GET' })
+                axios({url: '/users/' + _this.id, method: 'GET' })
                 .then((resp) => {
-                    _this.data = resp.data.records
+                    if (resp.data.result) {
+                        _this.data = resp.data.records
+                    } else {
+                        _this.showToast('error', resp.data.message)
+                        _this.CLOSE()
+                    }
                 })
                 .catch((err) => {
+                    _this.CLOSE()
+                    _this.showToast()
                 })
             }
             return _this.open
@@ -127,18 +162,61 @@ export default {
 				title: message
 			})
         },
+        showLoader(show = false) {
+			if (show) {
+				this.loader = this.$loading.show({
+					container:  null,
+					canCancel: false,
+					color: '#5E72E4',
+					backgroundColor: '#808080'
+				})
+			} else {
+				this.loader.hide()
+			}
+        },
+        loadData(url = '') {
+            let _this = this
+            axios({url: url , method: 'GET'})
+			.then((resp) => {
+			    if(resp.data.records.length > 0) {
+                    let records = resp.data.records
+                    switch(url) {
+                    case 'rols':
+                        _this.roles = records
+                        break;
+                    case 'departments':
+                        _this.departments = records
+                        break;
+                    case 'regions':
+                        _this.regions = records
+                        break;
+                    default:
+                        _this.roles = records
+                    } 
+					_this.icon = 'success'
+					_this.message = resp.data.message
+				} else {
+					_this.icon = 'error'
+					_this.message = 'No existen ' + url + ' registrados'
+                    _this.CLOSE()
+				}
+				_this.showToast(_this.icon, _this.message)
+			}).catch((err) => {
+				_this.showToast(_this.icon)
+                _this.CLOSE()
+			})
+        },
         CLOSE: function(){
             this.$emit('close')
         },
         SEND: function(){
             let _this = this
-            this.count++
-            this.load = true
+            _this.count++
+            _this.load = true
 
-            let icon = 'error'
-            let method = this.method == 'PUT' ? '/' + this.data.id : ''
+            let method = _this.method == 'PUT' ? '/' + _this.data.id : ''
 
-            if(this.count == 1){
+            if(_this.count == 1){
                 let form = new FormData()
                 $.each(this.data, function(key, item) {
                     if(item != null){
@@ -153,18 +231,19 @@ export default {
 					axios({url: '/users' + method, method: 'POST', data: form })
 					.then((resp) => {
 						if(resp.data.result) {
-							icon = 'success'
+							_this.icon = 'success'
 							_this.message = resp.data.message
                             _this.CLOSE()
-                            _this.showToast(icon, _this.message)
+                            _this.showToast(_this.icon, _this.message)
 						} else {
+                            _this.icon = 'error'
                             _this.message = resp.data.message.split("(")[0]
-                            _this.showToast(icon, _this.message)
+                            _this.showToast(_this.icon, _this.message)
 						}
                         _this.load = false
                         _this.count = 0
 					}).catch((err) => {
-						_this.showToast(icon)
+						_this.showToast()
 					})
 				}, 1000)
             }
