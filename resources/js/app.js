d9f403506494b4f24ad55c91d7c68c1399085abf
Cristian González <crisgon1065@gmail.com>
2022-09-07 20:14:06 -0600

    Implementacion de axios en app para consumo de servicios

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from 'axios';
+
+window.axios = axios
+axios.defaults.baseURL = '/api/'
 
 createApp(App).use(router).mount("#app");
