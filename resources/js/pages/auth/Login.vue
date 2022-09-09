9404df2e20ad925950147a59e6585a36bfe04370
Cristian González <crisgon1065@gmail.com>
2022-09-09 14:29:39 -0600

    Flujo completo autenticacion

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -1,104 +1,122 @@
 <template>
-    <main class="main-content  mt-0">
-    <section>
-      <div class="page-header min-vh-100">
-        <div class="container">
-          <div class="row">
-            <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
-              <div class="card card-plain">
-                <div class="card-header pb-0 text-start">
-                  <h4 class="font-weight-bolder">Sign In</h4>
-                  <p class="mb-0">Enter your email and password to sign in</p>
-                </div>
-                <div class="card-body">
-                  <form role="form"  @submit.prevent="SEND_DATA()">
-                    <div class="mb-3">
-                      <input type="email" class="form-control form-control-lg" placeholder="Email" aria-label="Email" v-model="data.email">
-                    </div>
-                    <div class="mb-3">
-                      <input type="password" class="form-control form-control-lg" placeholder="Password" aria-label="Password" v-model="data.password">
-                    </div>
-                    <div class="text-center">
-                      <button type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Sign in</button>
-                    </div>
-                  </form>
-                </div>
-                <div class="card-footer text-center pt-0 px-lg-2 px-1">
-                  <p class="mb-4 text-sm mx-auto">
-                    Don't have an account?
-                    <a href="javascript:;" class="text-primary text-gradient font-weight-bold">Sign up</a>
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
-              <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-ill.jpg');
-          background-size: cover;">
-                <span class="mask bg-gradient-primary opacity-6"></span>
-                <h4 class="mt-5 text-white font-weight-bolder position-relative">"Attention is the new currency"</h4>
-                <p class="text-white position-relative">The more effortless the writing looks, the more effort the writer actually put into the process.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </main>
+    <main class="main-content mt-0">
+		<section>
+			<div class="page-header min-vh-95">
+				<div class="container">
+					<div class="row">
+						<div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0 mx-auto">
+							<div class="card card-plain">
+								<div class="card-header pb-0 text-start">
+									<h4 class="font-weight-bolder">Iniciar Sesión</h4>
+									<p class="mb-0">Ingresa tus credenciales para acceder</p>
+								</div>
+								<div class="card-body">
+									<form role="form"  @submit.prevent="login()">
+										<div class="mb-3">
+											<input type="email" class="form-control form-control-lg" placeholder="Email" v-model="data.email">
+										</div>
+										<div class="mb-3">
+											<input type="password" class="form-control form-control-lg" placeholder="Contraseña" v-model="data.password">
+										</div>
+										<div class="text-center">
+											<button type="submit" class="btn btn-lg btn-primary btn-lg w-100 mt-4 mb-0">Iniciar Sesión</button>
+										</div>
+									</form>
+								</div>
+							</div>
+						</div>
+						<div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 end-0 text-center justify-content-center flex-column">
+							<div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-ill.jpg'); background-size: cover;">
+								<span class="mask bg-gradient-primary opacity-6"></span>
+								<h4 class="mt-5 text-white font-weight-bolder position-relative">Sistema de Control de Personal</h4>
+								<p class="text-white position-relative">Gestiona a los empleados y administra información de personal.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
 </template>
 <script>
-    export default {
-        data(){
-            return{
-                data: {
-                    username: '',
-                    password: '',
-                },
-                errors: {
-                    username: '',
-                    password: '',
-                },
-                load: false,
-                count: 0
-            }
+import axios from "axios";
+
+export default {
+    data(){
+        return {
+			count: 0,
+			icon: '',
+            message: '',
+			loader: {},
+            data: {
+                email: '',
+                password: '',
+            },
+        }
+    },
+    methods:{
+		showToast(icon = "error", message = "Ocurrió un error, por favor vuelva a intentar") {
+            const Toast = this.$swal.mixin({
+				toast: true,
+				position: 'top-end',
+				showConfirmButton: false,
+				timer: 3000,
+				timerProgressBar: true,
+				didOpen: (toast) => {
+					toast.addEventListener('mouseenter', this.$swal.stopTimer)
+					toast.addEventListener('mouseleave', this.$swal.resumeTimer)
+				}
+			})
+			Toast.fire({
+				icon: icon,
+				title: message
+			})
         },
-        methods:{
-            SEND_DATA: function(){
-                this.count++
-                this.load = true
-                this.errors = []
-                if(this.count == 1){
-                    axios({url: '/login', method: 'POST', data: this.data })
-                    .then((resp) => {
-                        console.log(resp.data.message)
-                        const token = resp.data.records.token
+        showLoader(show = false) {
+			if (show) {
+				this.loader = this.$loading.show({
+					container:  null,
+					canCancel: false,
+					color: '#5E72E4',
+					backgroundColor: '#808080'
+				})
+			} else {
+				this.loader.hide()
+			}
+        },
+        login() {
+			let _this = this
+            _this.count++
+            if(_this.count == 1) {
+				_this.showLoader(true)
+                axios({url: '/login', method: 'POST', data: this.data })
+                .then((resp) => {
+					if(resp.data.result) {
+						const token = resp.data.records.token
                         localStorage.setItem('token', token)
-                        console.log( localStorage.getItem('token'))
-                        // this.$store.dispatch('INIT_USER', token)
-                        // setTimeout(() => {
-                        //     this.load = false
-                        //     this.count = 0
-                        //     axios({url: '/info', method: 'GET' })
-                        //     .then((resp) => {
-                        //         localStorage.setItem('user', JSON.stringify(resp.data))
-                        //         this.$router.go({ name: 'Home' })
-                        //         window.location.reload('/admin/panel')
-                        //     }).catch((err) => {
-                        //     })
-                        // }, 1000)
-                    }).catch((err) => {
-                        if(err.response.status === 422){
-                            setTimeout(() => {
-                                this.errors = err.response.data
-                                this.count = 0
-                                this.load = false
-                            }, 750)
-                        }else{
-                            this.count = 0
-                            this.load = false
-                        }
-                    })
-                }
+						this.$router.push({ name: 'Home' })
+						window.location.reload('/')
+					} else {
+                        _this.icon = 'error'
+						_this.message = resp.data.message
+						_this.showToast(_this.icon, _this.message)
+					}
+                    _this.count = 0
+					_this.showLoader(false)
+                }).catch((err) => {
+					let status = err.response.status
+					if (status === 422 || status === 401) {
+						_this.icon = 'error'
+						_this.message = 'Credenciales incorrectas'
+						_this.showToast(_this.icon, _this.message)
+                    } else {
+						_this.showToast()
+                    }
+					_this.count = 0
+					_this.showLoader(false)
+                })
             }
         }
     }
-    </script>
+}
+</script>
--- resources/js/pages/partials/Navbar.vue
+++ resources/js/pages/partials/Navbar.vue
@@ -1,25 +1,49 @@
 <template>
     <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur" data-scroll="false">
-      <div class="container-fluid py-1 px-3">
-        <nav aria-label="breadcrumb">
-          <h6 class="font-weight-bolder text-white mb-0">Dashboard</h6>
-        </nav>
-        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
-          <div class="ms-md-auto pe-md-3 d-flex align-items-center">
-            <div class="input-group">
-              <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
-              <input type="text" class="form-control" placeholder="Type here...">
-            </div>
-          </div>
-          <ul class="navbar-nav  justify-content-end">
-            <li class="nav-item d-flex align-items-center">
-              <router-link :to="{ name: 'Profile' }" class="nav-link text-white font-weight-bold px-0">
-                <i class="fa fa-user me-sm-1"></i>
-                <span class="d-sm-inline d-none">Perfil</span>
-              </router-link>
-            </li>
-          </ul>
-        </div>
-      </div>
+      	<div class="container-fluid py-1 px-3">
+        	<nav aria-label="breadcrumb">
+          		<h6 class="font-weight-bolder text-white mb-0">Dashboard</h6>
+        	</nav>
+        	<div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+          	
+          	<ul class="navbar-nav ms-md-auto justify-content-end">
+            	<li class="nav-item d-flex align-items-center">
+					<router-link :to="{ name: 'Profile' }" class="nav-link text-white font-weight-bold px-0">
+						<i class="fa fa-user me-sm-2"></i>
+						<span class="d-sm-inline d-none">Perfil</span>
+					</router-link>
+            	</li>
+				<li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+					<a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+						<div class="sidenav-toggler-inner">
+							<i class="sidenav-toggler-line bg-white"></i>
+							<i class="sidenav-toggler-line bg-white"></i>
+							<i class="sidenav-toggler-line bg-white"></i>
+						</div>
+					</a>
+				</li>
+				<li class="nav-item px-4 d-flex align-items-center">
+					<a @click="logout()" class="nav-link text-white font-weight-bold px-0 cursor-pointer">
+						<i class="fa fa-power-off me-sm-2"></i>
+						<span class="d-sm-inline d-none">Cerrar Sesión</span>
+					</a>
+				</li>
+          	</ul>
+        	</div>
+      	</div>
     </nav>
 </template>
+<script>
+	export default {
+		methods: {
+			logout() {
+				axios({url: 'logout', method: 'POST' })
+				.then((resp) => {
+					localStorage.removeItem('token')
+					this.$router.push({ name: 'Login' })
+					window.location.reload('/login')
+				})
+			}
+		}
+	}
+	</script>
