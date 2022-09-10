9675ff14402d5c78ae1ce7a03b351f144fc01d07
Cristian González <crisgon1065@gmail.com>
2022-09-09 22:25:44 -0600

    Ajuste importacion de imagen logo

--- resources/js/pages/partials/Sidenav.vue
+++ resources/js/pages/partials/Sidenav.vue
@@ -3,7 +3,7 @@
     <div class="sidenav-header">
       	<i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       	<router-link class="navbar-brand m-0" :to="{ name: 'Home' }">
-      	  	<img src="/img/logoRH.png" class="navbar-brand-img h-100" alt="main_logo">
+      	  	<img src="img/logoRH.png" class="navbar-brand-img h-100" alt="main_logo">
       	  	<span class="ms-1 font-weight-bold">Control de Personal</span>
       	</router-link>
     </div>
